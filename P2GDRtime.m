bacc232806bff3d6b88f4e8a3931591b275950e3
Guillaume Olikier <guillaume.olikier@gmail.com>
2025-11-14 17:56:51 +0100

    Update P2GDRtime.m

--- P2GDRtime.m
+++ P2GDRtime.m
@@ -1,6 +1,6 @@
-function [s, U, V, f, time] = P2GDRtime(r, s, U, V, g0, g1, a, b, c, Delta, f_tol)
+function [s, U, V, f, time, i] = P2GDRtime(r, s, U, V, g0, g1, a, b, c, Delta, f_tol)
 %% Description
-% Author: Guillaume Olikier (2025-06-13)
+% Author: Guillaume Olikier (2025-10-12)
 % This function implements P2GDR [OGA24, Definition 6.1].
 % Input:
 %   - a positive integer r;
@@ -17,11 +17,14 @@ function [s, U, V, f, time] = P2GDRtime(r, s, U, V, g0, g1, a, b, c, Delta, f_to
 % Output:
 %   - the first (s, U, V) such that g0(U.*s, V) <= f_tol;
 %   - the value f of g0 at (U.*s, V);
-%   - the running time required by P2GDR to generate (s, U, V).
+%   - the running time required to generate (s, U, V);
+%   - the number of iterations required to generate (s, U, V).
 % This function computes only what is necessary to generate (s, U, V).
 tic
 f = g0(U.*s, V);
-while f > f_tol
+i = 0;
+while f > f_tol && toc <= 60
     [s, U, V, f] = P2GDRmap(r, s, U, V, g0, g1, a, b, c, Delta);
+    i = i+1;
 end
 time = toc;
